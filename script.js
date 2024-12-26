16b8e4a4d85b578a1ec2d3a6428c3b9944589f55
Aaron Ramos <122333150+aar0m@users.noreply.github.com>
2024-12-25 23:57:11 -1000

    finish experience section

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function swapDiv(ele) {
-    // replace y2024 with find element window.alert(document.getElementById('Y2024').parentElement.parentElement.id);
-
     const otherYears = document.querySelectorAll(".exp-txt");
 
     otherYears.forEach(year => {
@@ -11,10 +9,45 @@ function swapDiv(ele) {
 
     otherYearLabels.forEach(yearLabel => {
         document.getElementById(yearLabel.id).style.fontWeight = '100';
+        document.getElementById(yearLabel.id).classList.toggle("label");
     });
 
+    document.getElementById("L".concat(ele.id)).classList.toggle("label");
     document.getElementById("L".concat(ele.id)).style.fontWeight = 'bold';
     document.getElementById("Y".concat(ele.id)).checked = true;
     document.getElementById("T".concat(ele.id)).style.animation = 'appear-slideUp 0.6s';
     document.getElementById("T".concat(ele.id)).style.display = 'block';
+}
+
+function swapRight() {
+    var years = document.querySelectorAll("input[name='year']");
+
+    var target = document.querySelector("input[name='year']:checked");
+    var rootNode = document.getElementById(target.id).parentElement.parentElement;
+    var rightNode = document.getElementById(rootNode.id - 1);
+
+    if (target.id == years[years.length-1].id) {
+        target = years[0];
+        rootNode = document.getElementById(target.id).parentElement.parentElement;
+        rightNode = document.getElementById(rootNode.id);
+    }
+
+    swapDiv(rightNode);
+}
+
+function swapLeft() {
+    var years = document.querySelectorAll("input[name='year']");
+
+    var target = document.querySelector("input[name='year']:checked");
+    var rootNode = document.getElementById(target.id).parentElement.parentElement;
+    var replacement = parseInt(rootNode.id[rootNode.id.length - 1]) + 1
+    var leftNode = document.getElementById(rootNode.id.replace(/.$/, replacement));
+
+    if (target == years[0]) {
+        target = years[years.length-1];
+        rootNode = document.getElementById(target.id).parentElement.parentElement;
+        leftNode = document.getElementById(rootNode.id);
+    }
+
+    swapDiv(leftNode);
 }
